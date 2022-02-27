33896cda9eea940bde2b33815fa324d229754bc1
Ievgenii Loza <ievgenii.loza@xeneta.com>
2022-02-27 18:11:09 +0100

    add visual breaks in logs

--- main.sh
+++ main.sh
@@ -5,7 +5,9 @@ addresses=`cat $sourceFile | sort -R`
 while true
 do
     for address in $addresses; do
+        echo "💥💥💥"
         echo "Pinging: $address"
+        echo "💥💥💥"
         curl -vk $address --output /dev/null --silent
     done
 done
