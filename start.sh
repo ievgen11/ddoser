eab75e6cfb5f97180a6968ef9b8ab23586674aa3
Ievgenii Loza <ievgenii.loza@xeneta.com>
2022-02-27 21:28:26 +0100

    add logger for all containers

--- start.sh
+++ start.sh
@@ -6,3 +6,9 @@ docker swarm init
 
 docker service create --name ddoser_service --detach=false ddoser:dockerfile
 docker service scale ddoser_service=100
+
+for name in $(docker ps --format "{{.Names}}"); do
+  eval "docker logs -f --tail=5 \"$name\" | sed -e \"s/^/[-- $name --] /\" &";
+done
+
+wait
